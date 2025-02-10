a96458705dfb2188c7aebd678d3f32984ad5f881
Nithin Mathew <nithin.mathew@sysfore.com>
2025-02-10 15:28:39 +0530

    Initial commit

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const Card = () => {
+
+  return (
+
+    <div>
+      <h2>Card Component</h2>
+    </div>
+
+  );
+
+}
+
+const App = () => {
+
+  return (
+    <>
+      <Card></Card>
+      <h2>Functional Arrow Component</h2>
+    </>
+
+  );
+
+}
+
+
+
+export default App
