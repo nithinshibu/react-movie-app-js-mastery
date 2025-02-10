eb69ed00612a94d843dc31fc12722535bfbab328
Nithin Mathew <nithin.mathew@sysfore.com>
2025-02-10 19:23:24 +0530

    movies api configuration

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,8 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./components/search";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: { accept: "application/json", Authorization: `Bearer ${API_KEY}` },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+    } catch (error) {
+      console.log(`Error fetching movies: ${error} `);
+      setErrorMessage("Error fetching movies.Please try again later.");
+    }
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <main>
       <div className="pattern"></div>
@@ -13,8 +35,13 @@ const App = () => {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   );
