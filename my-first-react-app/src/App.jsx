83a10f4aee1652d34efd9cf43d4856637322b7ce
Nithin Mathew <nithinshibumathew@gmail.com>
2025-02-11 12:13:42 +0530

    loader added

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/search";
+import Spinner from "./components/spinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -62,9 +63,9 @@ const App = () => {
         </header>
 
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]">All Movies</h2>
           {isLoading ? (
-            <p className="text-white">Loading...</p>
+            <Spinner />
           ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
           ) : (
